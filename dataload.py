867082b5aebdb59340c7266c7503152201a7c2a6
Robert <clearnorthanalytics@gmail.com>
2026-01-07 22:07:29 -0500

    improved database connection and json file to db flow

--- dataload.py
+++ dataload.py
@@ -2,14 +2,26 @@
 
 # import packages
 import os
-import psycopg2
+import psycopg3
 import re
 import urllib.request, urllib.parse, urllib.error
 import http
 import json
 import ssl
+import logging
 
-# base URL
+# -------------------------------------------------------------------
+# Logging setup
+# -------------------------------------------------------------------
+logging.basicConfig(
+            level=logging.INFO,
+                format="%(asctime)s [%(levelname)s] %(message)s"
+                )
+
+
+# -------------------------------------------------------------------
+# base URL processing
+# -------------------------------------------------------------------
 serviceurl = 'https://www150.statcan.gc.ca/t1/wds/rest/getFullTableDownloadCSV/'
 
 tbl = input('Enter CANSIM NDM table number: ')
@@ -31,7 +43,6 @@ with urllib.request.urlopen(url) as uh:
 
 print(newurl)
 
-
 postbody = json.dumps([{"productId": int(tbl)}]).encode()
 
 print(postbody)
@@ -46,15 +57,89 @@ req = urllib.request.Request(metaurl, data=postbody,
 with urllib.request.urlopen(req) as uh:
     data = json.loads(uh.read().decode())
     data = data[0]['object']
-    print(data.keys())
 
-    
-    for k,v in data.items():
-        if type(v) == list:
-            print(k)
-            for i in v:
-                print(type(i))
-        else:
-            print(k, type(v))
+# parse json file:
+#    for k,v in data.items():
+#        if type(v) == list:
+#            print(k)
+#            for i in v:
+#                print(type(i))
+#        else:
+#            print(k, type(v))
 
-    #print(type(data[0].keys))
+
+
+# -------------------------------------------------------------------
+# parse metadata and load data into staging area of database
+# -------------------------------------------------------------------
+
+# make tuples of the items I would like to capture from the metadata table
+tblKeys = ('productId', 'cubeTitleEn', 'cubeTitleFr', 'cubeStartDate', 'cubeEndDate', 'releaseTime')
+dimKeys = ('dimensionPositionId', 'dimensionNameEn', 'dimensionNameFr')
+memberKeys = ('memberId', 'parentMemberId', 'memberNameEn', 'memberNameFr', 'terminated')
+
+tblmeta = tuple(data[k] for k in tblKeys)
+
+cur.execute('''INSERT INTO load.Cube (productId, cubeTitleEn, cubeTitleFr, cubeStartDate, cubeEndDate, releaseTime)
+                    VALUES ( ?, ?, ?, ?, ?, ? )''', tblKeys)
+
+# -------------------------------------------------------------------
+# Database connection and prep the staging area for data loading
+# -------------------------------------------------------------------
+user = os.getenv("POSTGRES_USER")
+password = os.getenv("POSTGRES_PASSWORD")
+db = os.getenv("POSTGRES_DB")
+#engine = create_engine(f"postgresql+psycopg://{user}:{password}@db:5432/{db}")
+conn_str = f"postgresql://{user}:{password}@db:5432/{db}"
+with psycopg.connect(conn_str) as conn:
+        with conn.cursor() as cur:
+            
+            cur.execute('''
+                        DROP TABLE IF EXISTS load.Cube;
+                        DROP TABLE IF EXISTS load.Dimensions;
+                        DROP TABLE IF EXISTS load.Members;
+
+                        CREATE TABLE Cube (
+                            productId INTEGER PRIMARY KEY, 
+                            cubeTitleEn TEXT, 
+                            cubeTitleFr TEXT, 
+                            cubeStartDate DATE, 
+                            cubeEndDate DATE, 
+                            releaseTime TIMESTAMP
+                        );
+
+                        CREATE TABLE Dimensions (
+                            dimensionPositionId INTEGER PRIMARY KEY, 
+                            dimensionNameEn TEXT, 
+                            dimensionNameFr TEXT
+                        );
+
+                        CREATE TABLE Members (
+                            dimensionPositionId INTEGER, 
+                            memberId INTEGER, 
+                            parentMemberId INTEGER, 
+                            memberNameEn TEXT, 
+                            memberNameFr TEXT, 
+                            terminated INTEGER) 
+
+                            PRIMARY KEY(dimensionPositionId, memberId)
+                        );
+
+                        ''')
+
+
+            for d in data['dimension']:
+
+                dd = tuple(d[k] for k in dimKeys)
+
+                dimensionPositionId = d['dimensionPostionId']
+
+                cur.execute('''INSERT INTO load.Dimensions (dimensionPositionId, dimensionNameEn, dimensionNameFr)
+                                VALUES ( ?, ?, ? )''', dimKeys)
+
+                for m in d['members']:
+                    mm = (dimensionPositionId,) +  tuple(m[k] for k in memberKeys)
+
+                cur.execute('''INSERT INTO load.Members (dimensionPositionId, memberId, parentMemberId, memberNameEn, memberNameFr, terminated)
+                                VALUES ( ?, ?, ?, ?, ?, ? )''', memberKeys)
+
